30594eff971afc05ec83b77840fbc0fcc4e53c70
Nikita_Kulchytsyi <zpaulz61@gmai.com>
2022-06-10 19:47:40 +0300

    more short code

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -3,31 +3,26 @@ import { controls } from '../../constants/controls';
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
     // resolve the promise with the winner when fight is over
+
     let firstFighterHealth = firstFighter.health;
     let secondFighterHealth = secondFighter.health;
     let firstFighterBlock = false;
     let secondFighterBlock = false;
     let firstFighterCrit = false;
     let secondFighterCrit = false;
+    const pressedKeys = new Set();
 
-    document.addEventListener('keyup', function (event) {
-      if (event.code === controls.PlayerOneAttack && !secondFighterBlock && !firstFighterBlock) {
+    function onKeyUp(event) {
+      const firstPlayerAttack = event.code === controls.PlayerOneAttack && !secondFighterBlock && !firstFighterBlock;
+      const secondPlayerAttack = event.code === controls.PlayerTwoAttack && !secondFighterBlock && !firstFighterBlock
+
+      if (firstPlayerAttack) {
         secondFighterHealth -= getDamage(firstFighter, secondFighter);
         updateHealthIndicator(secondFighter, secondFighterHealth, 'right');
-        console.log(secondFighterHealth);
       }
-      if (event.code === controls.PlayerTwoAttack && !secondFighterBlock && !firstFighterBlock) {
+      if (secondPlayerAttack) {
         firstFighterHealth -= getDamage(secondFighter, firstFighter);
-        updateHealthIndicator(firstFighter, firstFighterHealth, 'left')
-        console.log(firstFighterHealth);
-      }
-
-      if (firstFighterHealth <= 0) {
-        resolve(secondFighter);
-      }
-
-      if (secondFighterHealth <= 0) {
-        resolve(firstFighter);
+        updateHealthIndicator(firstFighter, firstFighterHealth, 'left');
       }
 
       if (event.code === controls.PlayerOneBlock) {
@@ -36,47 +31,64 @@ export async function fight(firstFighter, secondFighter) {
       if (event.code === controls.PlayerTwoBlock) {
         secondFighterBlock = false;
       }
-    });
 
-    //Block-------------
-    document.addEventListener('keypress', function (event) {
+      pressedKeys.delete(event.code);
+
+      if (firstFighterHealth <= 0) {
+        resolve(secondFighter);
+        document.removeEventListener('keyup', onKeyUp);
+        document.removeEventListener('keyup', onKeyPress);
+        document.removeEventListener('keydown', onKeyDown);
+      }
+      if (secondFighterHealth <= 0) {
+        resolve(firstFighter);
+        document.removeEventListener('keyup', onKeyUp);
+        document.removeEventListener('keyup', onKeyPress);
+        document.removeEventListener('keydown', onKeyDown);
+      }
+    }
+
+    function onKeyPress(event) {
       if (event.code === controls.PlayerOneBlock) {
         firstFighterBlock = true;
       }
       if (event.code === controls.PlayerTwoBlock) {
         secondFighterBlock = true;
       }
-    });
+    }
 
-    //Ulta-------------
-    const mySet = new Set();
-    document.addEventListener('keydown', function (event) {
-      mySet.add(event.code);
+    function onKeyDown(event) {
+      const firstPlayerCritHit = controls.PlayerOneCriticalHitCombination.every((key) => pressedKeys.has(key)) && !firstFighterCrit
+      const secondPlayerCritHit = controls.PlayerTwoCriticalHitCombination.every((key) => pressedKeys.has(key)) && !secondFighterCrit
 
-      if (controls.PlayerOneCriticalHitCombination.every((key) => mySet.has(key)) && !firstFighterCrit) {
+      pressedKeys.add(event.code);
+
+      if (firstPlayerCritHit) {
         secondFighterHealth -= getCritHit(firstFighter);
-        updateHealthIndicator(secondFighter, secondFighterHealth, 'right')
+        updateHealthIndicator(secondFighter, secondFighterHealth, 'right');
+
         firstFighterCrit = true;
         setTimeout(() => {
           firstFighterCrit = false;
         }, 10000);
-        console.log('Ulta!');
       }
 
-      if (controls.PlayerTwoCriticalHitCombination.every((key) => mySet.has(key)) && !secondFighterCrit) {
+      if (secondPlayerCritHit) {
         firstFighterHealth -= getCritHit(secondFighter);
-        updateHealthIndicator(firstFighter, firstFighterHealth, 'left')
+        updateHealthIndicator(firstFighter, firstFighterHealth, 'left');
+
         secondFighterCrit = true;
         setTimeout(() => {
           secondFighterCrit = false;
         }, 10000);
-        console.log('Ulta!');
       }
-    });
+    }
 
-    document.addEventListener('keyup', function (event) {
-      mySet.delete(event.code);
-    });
+    document.addEventListener('keyup', onKeyUp);
+    document.addEventListener('keypress', onKeyPress);
+    document.addEventListener('keydown', onKeyDown);
+
+    //Ulta-------------
   });
 }
 
@@ -109,5 +121,5 @@ function updateHealthIndicator(fighter, currentHealth, position) {
   const healthBar = document.getElementById(`${position}-fighter-indicator`);
   const healthBarWidth = (100 / health) * currentHealth;
 
-  healthBar.style.width = `${healthBarWidth}%`;
+  healthBar.style.width = healthBarWidth < 0 ? 0 : `${healthBarWidth}%`;
 }
