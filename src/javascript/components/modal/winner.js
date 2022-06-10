0f55f286cfd632ef70f23882085f058abbfc2f7b
Nikita_Kulchytsyi <zpaulz61@gmai.com>
2022-06-10 17:26:43 +0300

    add all finction

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,11 @@
+import { createFighterImage } from '../fighterPreview';
+import { showModal } from './modal';
+
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  // call showModal function
+  showModal({
+    title: 'Winner',
+    bodyElement: createFighterImage(fighter),
+    onClose: () => window.location.reload()
+  });
 }
